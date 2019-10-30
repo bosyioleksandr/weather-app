c10be3d52085adf8df62cc5f89c466e1f33a39c6
bosyioleksandr <whiteshinigami007@gmail.com>
2019-10-31 02:14:16 +0300

    WeatherApp task-1: create weather.service

--- weather-project/src/app/app.component.ts
+++ weather-project/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from './weather/weather.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'weather-project';
+
+  constructor(private weatherService: WeatherService) {
+
+  }
+
+  ngOnInit(): void {
+    this.weatherService.getWeatherForCurrentDay().subscribe(data => console.log(data));
+  }
+
+
 }
